655efeefe097f726aece34e1115ff6e877342520
Tomas <tosanchez@frba.utn.edu.ar>
2021-05-14 22:26:34 -0300

    refactor(montoExtraido): utilizar encapsulamiento de movimeintos

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -113,7 +113,7 @@ public class Cuenta {
 
   public double getMontoExtraidoA(LocalDate fecha) {
     return getMovimientos().stream()
-        .filter(movimiento -> !movimiento.isDeposito() && movimiento.getFecha().equals(fecha))
+        .filter(movimiento -> movimiento.isDeposito() && movimiento.esDeLaFecha(fecha))
         .mapToDouble(Movimiento::getMonto).sum();
   }
 
